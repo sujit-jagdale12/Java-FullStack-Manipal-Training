ca6ae2d44e87807daf0f8a4be71d3050242efa81
sujit-jagdale12 <sujitjagdale12@gmail.com>
2023-04-15 12:35:58 +0530

    spring-boot : JPA data

---
+++ SpringBoot-Projects/spring-jpa-data/src/test/java/com/ani/data/springjpadata/SpringJpaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.data.springjpadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
