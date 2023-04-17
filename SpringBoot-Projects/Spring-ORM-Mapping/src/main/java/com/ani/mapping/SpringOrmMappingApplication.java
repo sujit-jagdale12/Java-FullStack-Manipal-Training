5bca6ef74c68d5425c2bd99b6cec5365554c4427
sujit-jagdale12 <sujitjagdale12@gmail.com>
2023-04-17 12:34:40 +0530

    spring : orm

---
+++ SpringBoot-Projects/Spring-ORM-Mapping/src/main/java/com/ani/mapping/SpringOrmMappingApplication.java
@@ -0,0 +1,13 @@
+package com.ani.mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrmMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrmMappingApplication.class, args);
+	}
+
+}
