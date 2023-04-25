2d8ee8360ebb7697ba755c127da8fb3148519682
sujit-jagdale12 <sujitjagdale12@gmail.com>
2023-04-25 12:06:40 +0530

    node-js : sync, async

---
+++ Node-JS/js-funda/app.js
@@ -0,0 +1,11 @@
+console.log("First node js program.")
+
+const AsciiTable = require('ascii-table')
+const table = new AsciiTable('A Title')
+table
+  .setHeading('', 'Name', 'Age')
+  .addRow(1, 'Bob', 52)
+  .addRow(2, 'John', 34)
+  .addRow(3, 'Jim', 83)
+ 
+console.log(table.toString())
