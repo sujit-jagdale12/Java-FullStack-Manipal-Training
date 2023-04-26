88d6190749cc23403bf9f7ffa9f1b28085323c2b
sujit-jagdale12 <sujitjagdale12@gmail.com>
2023-04-26 12:32:55 +0530

    nodejs : callback,promise

---
+++ Node-JS/eventemmiter/event.js
@@ -0,0 +1,20 @@
+import { EventEmitter } from 'node:events';
+
+const emitter = new EventEmitter() // creating
+
+const onTick = dt => console.log('✅ Called =>  ' + dt) // es6 => arrow function
+
+function onTickNormal(dt) { // normal function
+    console.log('✅ Called normal ' + dt)
+}
+
+const onComplete = () => console.log(`✅ Files Uploaded`)
+
+emitter.on('tick', onTick) // listening -> method reference 
+emitter.on('tick', onTickNormal) // listening
+
+emitter.once('completed', onComplete)
+
+emitter.emit('tick', 1) // emitting
+
+emitter.off('tick', onTick) // removing event listener
