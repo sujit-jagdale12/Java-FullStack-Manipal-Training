f4a041256c7c2312e0673baee83bff884b69c281
sujit-jagdale12 <sujitjagdale12@gmail.com>
2023-04-29 11:58:44 +0530

    nodejs : custom query

--- 52-day-orm/type-orm/index.js
+++ 52-day-orm/type-orm/index.js
@@ -33,6 +33,12 @@ dataSource.initialize().then(() => {
 
     // categoryRepository.delete(2).then(res => console.log(`✅ Deleted Successfully`)).catch(console.log)
 
+    categoryRepository.query('select * from categories')
+        .then(ctgs => console.log(ctgs))
+        .catch(console.log)
+
+    const categories = categoryRepository.createQueryBuilder('Category').where('Category.name like :nm', { nm: '%P%' })
+    categories.getMany().then(ctgs => console.log(ctgs))
 
 }).catch(error => {
     console.log("❌ Error: ", error)
